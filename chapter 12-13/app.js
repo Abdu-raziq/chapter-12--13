82a78008f91de49ae54d2c15bccf40d789900e01
Abdul Raziq <154526495+Abdu-raziq@users.noreply.github.com>
2024-05-09 22:15:35 +0500

    Add files via upload

---
+++ chapter 12-13/app.js
@@ -0,0 +1,102 @@
+// // Question no 1
+// let input = prompt("Enter a alphabet or number:");
+// let inputchar = input.charCodeAt(0);
+// if(input >= '0' && input <= '9'){
+//     console.log(`${input} is a number`);
+//     Swal.fire(`${input} is a number!!!`);
+// }
+// else if (inputchar >= 65 && inputchar <= 90){
+//     console.log(`${input} is a Uppercase`);
+//     Swal.fire(`${input} is a Uppercase`);
+// }
+// else if (inputchar >= 97 && inputchar <= 122){
+//     console.log(`${input} is a Lowercase`);
+//     Swal.fire(`${input} is a Lowercase`);
+// }
+// else{
+//     console.log(`The character ${input} is not a number, uppercase letter, or lowercase letter.`);
+//     Swal.fire(`The character ${input} is not a number, uppercase letter, or lowercase letter.`);
+// }
+
+// Question no 2
+// let int_1=+prompt("Enter number 1:");
+// let int_2=+prompt("Enter number 2:");
+// if(int_1 > int_2){
+//     Swal.fire(`Number 1 =${int_1}`);
+//     Swal.fire(`Number 2 =${int_2}`);
+//     Swal.fire(`${int_1} is greater than ${int_2}`);
+// }
+// else if(int_1 < int_2){
+//     Swal.fire(`Number 1 =${int_1}`);
+//     Swal.fire(`Number 2 =${int_2}`);
+//     Swal.fire(`${int_1} is less than ${int_2}`);
+// }
+// else if(int_1 === int_2){
+//     Swal.fire(`Number 1 =${int_1}`);
+//     Swal.fire(`Number 2 =${int_2}`);
+//     Swal.fire(`${int_1} is equal to ${int_2}`);
+// }
+// else{
+//     Swal.fire(`Invalid!!!`);
+
+// }
+
+
+// Question no 3
+// let number =+prompt("Enter a number:");
+// if(number < 0){
+//     Swal.fire(`${number} is negative`);
+// }
+// else if(number > 0){
+//     Swal.fire(`${number} is positive`);
+// }
+// else if(number === 0){
+//     Swal.fire(`${number} is Zero`);
+// }
+// else{
+//     Swal.fire(`Invalid!!!`);
+// }
+
+// Question no 4
+// let string_input=prompt("Enter a alphabet:");
+// let alphabet1 = "a";
+// let alphabet2 = "e";
+// let alphabet3 = "i";
+// let alphabet4 = "o";
+// let alphabet5 = "u";
+// if(string_input === alphabet1 || string_input === alphabet2 || string_input === alphabet3 || string_input === alphabet4 || string_input === alphabet5){
+//     Swal.fire(`Yes this is true because ${string_input} is vowel`);
+// }
+// else{
+//     Swal.fire(`Invalid!!!`);
+// }
+
+
+// Question no 5
+// let password="Raziq123"
+// let userpassword=prompt("Enter your password");
+
+// if(userpassword.trim() ===""){
+//     Swal.fire(`Please enter your password!!!`);
+// }
+// else if(userpassword === "Raziq123"){
+//     Swal.fire(`Correct!!!`);
+// }
+// else{
+//     Swal.fire(`Incorrect!!!`);
+// }
+
+// Question no 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// console.log(greeting);
+// }
+// else {
+// greeting = "Good evening";
+// console.log(greeting);
+// }
+
+// Question no 7
+
